be727a3f41427c20b1e217843daf8eb58e1a2735
Kanza98 <kanza.98live.dk>
2020-05-11 17:09:52 +0200

    added some methods

--- src/controllers/learn_rainController.java
+++ src/controllers/learn_rainController.java
@@ -1,4 +1,11 @@
 package controllers;
 
+import java.awt.event.ActionEvent;
+import java.io.IOException;
+
 public class learn_rainController {
+
+    public void Button (ActionEvent actionEvent) throws IOException{
+
+    }
 }
